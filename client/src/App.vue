d855944637209cd006ca7b8e7bc4b9e7840ec8dd
Philip Oka <philip.oka@gmail.com>
2022-03-21 01:02:26 -0300

    - Creating a Vite App with Vue, Tailwind, Headless UI, Hero Icons and Vue Router; - Implemeting BaseInput and BaseButton components;

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { defineComponent } from '@vue/runtime-core'
+import { RouterView } from 'vue-router'
+
+export default defineComponent({
+  components: {
+    RouterView
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1
+          class="text-3xl font-bold leading-tight text-gray-900"
+        >
+        </h1>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ client/src/components/BaseInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="relative">
+  <input :placeholder = "label" class="h-14 w-64 peer
+      placeholder-transparent bg-gray-100 border-none
+      rounded-lg focus:ring-2 focus:ring-blue-500"
+      :type="type"
+      :value="modelValue"
+      v-bind="$attrs"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+
+    <label class="peer-placeholder-shown:text-base
+      peer-placeholder-shown:text-gray-400
+      peer-placeholder-shown:top-4
+      transition-all
+      peer-focus:top-1
+      peer-focus:text-gray-500
+      peer-focus:text-xs
+    text-gray-500 absolute left-3 top-1 text-xs"
+    >
+      {{ label }}
+    </label>
+    <span v-if="error" class="text-red-500">{{ error }}</span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    error: {
+      type: String
+    }
+  }
+});
+
+</script>
