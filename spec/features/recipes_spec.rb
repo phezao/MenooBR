cea554bab86fd261330aff88f6e6d5a16c168972
Philip Oka <philip.oka@gmail.com>
2023-02-12 18:23:43 -0300

    - Improving specs to other scenarios which you already have ingredients.

--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Recipes', type: :feature do
-  scenario 'Add a recipe' do
+  scenario 'Add the first recipe successfully' do
     visit root_path
 
     recipe_name = 'Tteokbokki com queijo gratinado e arroz'
@@ -9,13 +9,30 @@ RSpec.feature 'Recipes', type: :feature do
     click_on 'new-recipe'
     fill_in 'recipe-title', with: recipe_name
     fill_in 'recipe-notes', with: 'Não pode utilizar mel no preparo pois ele altera o sabor da comida'
-    fill_in(class: 'ingredient-1', with: 'Tteok')
-    click_on 'add-ingredient'
+    click_on 'add-new-ingredient'
+    fill_in 'ingredient-name', with: 'Tteok'
+    click_on 'add-more-ingredients'
     fill_in 'ingredient-2', with: 'Gochujang'
     click_on 'add-ingredient'
-    fill_in 'ingredient-3', with: 'Melado de cana'
-
     click_on 'submit-recipe'
-    expect(page).to include(recipe_name)
+    expect(page).to have_text(recipe_name)
+  end
+
+  scenario 'Add another recipe successfully' do
+    create(:ingredient, name: 'Arroz')
+    create(:ingredient, name: 'Polenta')
+    visit root_path
+
+    recipe_name = 'Arroz, polenta e frango grelhado'
+
+    click_on 'new-recipe'
+    fill_in 'recipe-title', with: recipe_name
+    select 'Arroz', from: 'ingredients-box'
+    select 'Polenta', from: 'ingredients-box'
+    click_on 'add-new-ingredient'
+    fill_in 'ingredient-name', with: 'Frango'
+    click_on 'add-ingredient'
+    click_on 'submit-recipe'
+    expect(page).to have_text(recipe_name)
   end
 end
