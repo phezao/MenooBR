ab601cf1aa4206acba5426fdb4a2f70f12038192
Philip Oka <philip.oka@gmail.com>
2023-02-12 18:09:18 -0300

    Implementing the feature: - Creating the resources for recipes in routes - Creating the Recipe model, ingredient model and join model/table, creating spec for validating the association.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'sites#index'
+  root 'recipes#index'
+  resources :recipes, only: %i[index new]
 end
